f8482ad87c36d8024806347561651b9259c0b2cd
Luis Vilcarromero <luis.evo_8.10@hotmail.com>
2021-09-05 08:44:01 -0500

    staticfiles

--- workshops/urls.py
+++ workshops/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import DefaultRouter
 from . import views as workshops_views
 
 router = DefaultRouter()
-router.register(r'workshops', workshops_views.WorkshopsViewSet, basename='workshops'),
-router.register(r'workshops/cover', workshops_views.WorkshopsCoverPageViewSet, basename='workshops-photo'),
-router.register(r'workshops/temary', workshops_views.WorkshopsTemaryViewSet, basename='workshops-photo'),
+router.register(r'courses', workshops_views.WorkshopsViewSet, basename='courses'),
+router.register(r'courses/cover', workshops_views.WorkshopsCoverPageViewSet, basename='courses-photo'),
+router.register(r'courses/temary', workshops_views.WorkshopsTemaryViewSet, basename='courses-temary'),
 
 urlpatterns = [
     path('', include(router.urls))
