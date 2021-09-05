19c0e1cec7de05d4fd6bfb667ecd955f732c4318
Luis Vilcarromero <luis.evo_8.10@hotmail.com>
2021-09-04 22:44:00 -0500

    without authentication

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -138,9 +138,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    #'DEFAULT_AUTHENTICATION_CLASSES': (
+    #    'rest_framework_simplejwt.authentication.JWTAuthentication',
+    #),
 }
 
 SIMPLE_JWT = {
